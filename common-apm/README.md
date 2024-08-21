9458afa715f77c3494b4ad4364eec27a6496e0f7
yamingdeng <yamingdeng@outlook.com>
2024-08-21 10:28:13 +0800

    add masking in Logback

---
+++ common-apm/README.md
@@ -0,0 +1,4 @@
+# Logging
+# Tracing
+# Profiling
+# Actuator
---
+++ common-apm/src/main/kotlin/com/example/demo/logging/MaskingPatternLayout.kt
@@ -0,0 +1,44 @@
+package com.example.demo.logging
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import ch.qos.logback.classic.PatternLayout;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+class MaskingPatternLayout : PatternLayout() {
+
+    private var multilinePattern: Pattern? = null
+    private val maskPatterns: MutableList<String> = ArrayList()
+
+    // invoked for every single entry in the xml
+    fun addMaskPattern(maskPattern: String) {
+        maskPatterns.add(maskPattern)
+        multilinePattern = Pattern.compile(maskPatterns.stream().collect(Collectors.joining("|")), Pattern.MULTILINE)
+    }
+
+    override fun doLayout(event: ILoggingEvent): String {
+        return maskMessage(super.doLayout(event))
+    }
+
+    private fun maskMessage(message: String): String {
+        if (multilinePattern == null) {
+            return message
+        }
+        val sb = StringBuilder(message)
+        val matcher: Matcher = multilinePattern!!.matcher(sb)
+        while (matcher.find()) {
+            IntStream.rangeClosed(1, matcher.groupCount()).forEach { group ->
+                if (matcher.group(group) != null) {
+                    // replace each character with asterisk
+                    IntStream.range(matcher.start(group), matcher.end(group)).forEach { i -> sb.setCharAt(i, '*') }
+                }
+            }
+        }
+        return sb.toString()
+    }
+}
